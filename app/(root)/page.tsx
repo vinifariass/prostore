52cb19f94729af7f93810371078e4f24c8231314
Vinicius Farias <fsvinicius9@gmail.com>
2025-02-27 10:10:58 -0300

    Creating product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,8 @@
+import sampleData from "./db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 const Homepage = async () => {
 
-  return <>Prostore</>;
+  return <div><ProductList data={sampleData.products} title="Newest Arrivals" limit={4} /></div>;
 }
 
 export default Homepage;
