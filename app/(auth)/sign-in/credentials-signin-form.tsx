fda6f45b2dad8ef2d197454ee6fb8df2d092b063
Vinicius Farias <fsvinicius9@gmail.com>
2025-02-28 13:42:52 -0300

    Creating signin page

---
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { signInDefaultValues } from "@/lib/constants";
+import Link from "next/link";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
+import { signInWithCredentials } from "@/lib/actions/user.action";
+
+const CredentialsSignInForm = () => {
+    const [data, action] = useActionState(signInWithCredentials, {
+        success: false,
+        message: ''
+    });
+
+const SignInButton = () => {
+    const { pending } = useFormStatus();
+    return (
+        <Button className="w-full" variant='default' disabled={pending}>
+            {pending ? 'Signing In...' : 'Sign In'}
+        </Button>
+    );
+}
+
+    return (
+        <form>
+            <div className="space-y-6">
+                <div>
+                    <Label htmlFor="email">Email</Label>
+                    <Input type="email" id="email" name="email" placeholder="Email" required autoComplete="email"
+                        defaultValue={signInDefaultValues.email} />
+                </div>
+
+                <div>
+                    <Label htmlFor="password">Password</Label>
+                    <Input type="password" id="password" name="password" placeholder="Password" required autoComplete="password"
+                        defaultValue={signInDefaultValues.password} />
+                </div>
+                <SignInButton />
+
+                {data && !data.success && (
+                    <div className="text-center text-destructive">
+                        {data.message} </div>
+                )}
+
+                <div className="text-sm text-center text-muted-foreground">
+                    Don&apos;t have and account? {' '}
+                    <Link href="/sign-up" target="_self" className="link">Sign Up</Link>
+                </div>
+            </div>
+        </form>);
+}
+
+export default CredentialsSignInForm;
--- lib/validators.ts
+++ lib/validators.ts
@@ -20,3 +20,10 @@ export const insertProductSchema = z.object({
     price: currency
 
 });
+
+//Schema for signing users in
+
+export const signInFormSchema = z.object({
+    email: z.string().email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters')
+});
