32cc2e76c8430f2d7f0188f915b3913503a8b6f0
Vinicius <fsvinicius9@gmail.com>
2025-03-08 09:40:45 -0300

    Create order action

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -6,6 +6,8 @@ import { auth } from "@/auth";
 import { getMyCart } from "./cart.actions";
 import { getUserById } from "./user.action";
 import { insertOrderSchema } from "../validators";
+import { prisma } from "@/db/prisma";
+import { CartItem } from "@/types";
 
 // Create order and create the order items
 
@@ -16,19 +18,19 @@ export async function createOrder() {
 
         const cart = await getMyCart();
         const userId = session?.user?.id;
-        if(!userId) throw new Error('User not found');
+        if (!userId) throw new Error('User not found');
 
         const user = await getUserById(userId);
         if (!cart || cart.items.length === 0) {
-            return {success:false, message: 'Your cart is empty',redirectTo: './cart'}
+            return { success: false, message: 'Your cart is empty', redirectTo: './cart' }
         }
 
         if (!user.address) {
-            return {success:false, message: 'No shipping address',redirectTo: './shipping-address'}
+            return { success: false, message: 'No shipping address', redirectTo: './shipping-address' }
         }
 
         if (!user.paymentMethod) {
-            return {success:false, message: 'No payment method',redirectTo: './payment-method'}
+            return { success: false, message: 'No payment method', redirectTo: './payment-method' }
         }
 
         //Create order object
@@ -44,6 +46,39 @@ export async function createOrder() {
         })
 
         // Create a transaction to create order and order items in the database
+
+        const insertedOrderId = await prisma.$transaction(async (tx) => {
+            //Create order
+
+            const insertedOrder = await tx.order.create({ data: order });
+            //Create order items from the cart items
+            for (const item of cart.items as CartItem[]) {
+                await tx.orderItem.create({
+                    data: {
+                        ...item,
+                        price: item.price,
+                        orderId: insertedOrder.id
+                    }
+                })
+            }
+            //Clear the cart
+            await tx.cart.update({
+                where: { id: cart.id },
+                data: {
+                    items: [],
+                    itemsPrice: 0,
+                    totalPrice: 0,
+                    shippingPrice: 0,
+                    taxPrice: 0
+                }
+            });
+            return insertedOrder.id;
+        })
+
+        if (!insertedOrderId) throw new Error('Order not created');
+
+        return ({ success: true, message: 'Order created', redirectTo: `/order/${insertedOrderId}` })
+
     } catch (err) {
         if (isRedirectError(err)) throw err
         return { success: false, message: formatErrors(err) }
