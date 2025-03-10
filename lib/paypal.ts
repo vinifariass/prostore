4f824c424ee5866519e51b5974beebfbfa00ea35
Vinicius <fsvinicius9@gmail.com>
2025-03-10 08:56:50 -0300

    Adding jest.config.js

---
+++ lib/paypal.ts
@@ -0,0 +1,25 @@
+const base = process.env.PAYPAL_API_URL || 'https://api-m.sandbox.paypal.com';
+
+export const paypal = {};
+
+//Generate paypal access token
+async function generateAccessToken(){
+    const {PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET} = process.env;
+    const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString('base64');
+
+    const response = await fetch(`${base}/v1/oauth2/token`, {
+        method: 'POST',
+        body: 'grant_type=client_credentials',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: `Basic ${auth}`
+        },
+    })
+
+    if(response.ok){
+        const jsonData = await response.json();
+        return jsonData.access_token;
+    } else {
+        throw new Error('Failed to generate access token');
+    }
+}
