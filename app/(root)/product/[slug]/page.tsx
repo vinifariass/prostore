3fc82d09a7808c10530a1dd57b59efb4541582de
Vinicius <fsvinicius9@gmail.com>
2025-03-11 11:45:03 -0300

    Fixing pagination function

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import { getProductBySlug } from "@/lib/actions/product.actions";
 import { notFound } from "next/navigation";
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import ProductPrice from "@/components/shared/product/product-price";
 import ProductImages from "@/components/shared/product/product-images";
--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -10,6 +10,7 @@ import { prisma } from "@/db/prisma";
 import { CartItem, PaymentResult } from "@/types";
 import { paypal } from "../paypal";
 import { revalidatePath } from "next/cache";
+import { PAGE_SIZE } from "../constants";
 
 // Create order and create the order items
 
@@ -140,7 +141,7 @@ export async function createPayPalOrder(orderId: string) {
 // Approve paypal order and update order to paid
 export async function approvePayPalOrder(orderId: string, data: { orderID: string }) {
     try {
-        
+
         const order = await prisma.order.findFirst({
             where: { id: orderId },
         })
@@ -225,4 +226,31 @@ async function updateOrderToPaid({
     });
 
     if (!updatedOrder) throw new Error('Order not found')
+}
+
+// Get user's orders
+export async function getMyOrders({
+    limit = PAGE_SIZE,
+    page
+}: {
+    limit?: number;
+    page: number;
+}) {
+    const session = await auth();
+    if (!session) throw new Error('User not authenticated');
+
+    const data = await prisma.order.findMany({
+        where: { userId: session?.user?.id! },
+        orderBy: { createdAt: 'desc' },
+        take: limit,
+        skip: (page - 1) * limit
+    })
+    const dataCount = await prisma.order.count({
+        where: { userId: session?.user?.id! }
+    })
+
+    return {
+        data,
+        totalPages: Math.ceil(dataCount / limit)
+    }
 }
