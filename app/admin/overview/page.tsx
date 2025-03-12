443f429d5298ba0ffe5d9f5f11e512d80fd1f729
Vinicius <fsvinicius9@gmail.com>
2025-03-12 10:05:09 -0300

    Protecting page

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -7,11 +7,14 @@ import { BadgeDollarSign, Barcode, CreditCard, Users } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
 import Charts from "./charts";
+import { requireAdmin } from "@/lib/auth-guard";
 
 export const metadata: Metadata = {
     title: 'Admin Overview',
 }
 const AdminOverviewPage = async () => {
+    await requireAdmin();
+
     const session = await auth();
 
     if (session?.user?.role !== 'admin') {
@@ -85,7 +88,7 @@ const AdminOverviewPage = async () => {
                     <CardContent>
                         <Charts data={{
                             salesData: summary.salesData,
-                        }}/>
+                        }} />
                     </CardContent>
                 </Card>
 
