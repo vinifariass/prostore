49281cff8ebcdd4607bbb78321734c3fce069305
Vinicius <fsvinicius9@gmail.com>
2025-03-14 16:56:57 -0300

    Fixing updateUser

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { shippingAddressSchema, signInFormSchema, singUpFormSchema } from "../validators";
+import { shippingAddressSchema, signInFormSchema, singUpFormSchema, updateUserSchema } from "../validators";
 import { auth, signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { hashSync } from "bcrypt-ts-edge";
@@ -210,12 +210,37 @@ export async function deleteUser(id: string) {
                 id
             }
         });
-        
+
         revalidatePath('/admin/users');
 
         return {
             success: true,
             message: 'User deleted successfully'
+        }
+
+    } catch (error) {
+        return { success: false, message: formatErrors(error) };
+    }
+}
+
+//Update a user
+export async function updateUser(user: z.infer<typeof updateUserSchema>) {
+    try {
+        await prisma.user.update({
+            where: {
+                id: user.id
+            },
+            data: {
+                name: user.name,
+                role: user.role
+            }
+        });
+
+        revalidatePath('/admin/users');
+
+        return {
+            success: true,
+            message: 'User updated successfully'
         }
 
     } catch (error) {
