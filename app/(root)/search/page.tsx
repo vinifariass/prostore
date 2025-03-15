02c4ccc09e3a4927e69661e3752776ecb4ceb7fc
Vinicius <fsvinicius9@gmail.com>
2025-03-15 15:08:51 -0300

    Adding reating and sort of newest filter

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,4 +1,5 @@
 import ProductCard from "@/components/shared/product/product-card";
+import { Button } from "@/components/ui/button";
 import { getAllCategories, getAllProducts } from "@/lib/actions/product.actions";
 import Link from "next/link";
 
@@ -56,6 +57,33 @@ const SearchPage = async (props: {
 
     const categories = await getAllCategories();
 
+    const prices = [
+        {
+            name: '$1 to $50',
+            value: '1-50'
+        },
+        {
+            name: '$51 to $200',
+            value: '51-200'
+        },
+        {
+            name: '$101 to $200',
+            value: '101-200'
+        },
+        {
+            name: '$201 to $500',
+            value: '201-500'
+        },
+        {
+            name: '$501 to $1000',
+            value: '501-1000'
+        },
+    ]
+
+    const ratings = [4, 3, 2, 1]
+
+    const sortOrders = ['newest', 'lowest', 'highest', 'rating']
+
     return (
         <div className='grid md:grid-cols-5 md:gap-5'>
             <div className='filter-links'>
@@ -72,11 +100,102 @@ const SearchPage = async (props: {
                                 Any
                             </Link>
                         </li>
+                        {categories.map((x) => (
+                            <li key={x.category}>
+                                <Link
+                                    className={`${category === x.category && 'font-bold'
+                                        }`}
+                                    href={getFilterUrl({ c: x.category })}
+                                >
 
+                                    {x.category}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                {/* Price Links */}
+                <div className='text-xl mb-2 mt-8'>Price</div>
+                <div>
+                    <ul className='space-y-1'>
+                        <li>
+                            <Link
+                                className={`${price === 'all' && 'font-bold'
+                                    }`}
+                                href={getFilterUrl({ p: 'all' })}
+                            >
+                                Any
+                            </Link>
+                        </li>
+                        {prices.map((p) => (
+                            <li key={p.value}>
+                                <Link
+                                    className={`${price === p.value && 'font-bold'
+                                        }`}
+                                    href={getFilterUrl({ p: p.value })}
+                                >
+
+                                    {p.value}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                {/* Rating Links */}
+                <div className='text-xl mb-2 mt-8'>Customer Ratings</div>
+                <div>
+                    <ul className='space-y-1'>
+                        <li>
+                            <Link
+                                className={`${rating === 'all' && 'font-bold'
+                                    }`}
+                                href={getFilterUrl({ p: 'all' })}
+                            >
+                                Any
+                            </Link>
+                        </li>
+                        {ratings.map((r) => (
+                            <li key={r}>
+                                <Link
+                                    className={`${rating === r.toString() && 'font-bold'
+                                        }`}
+                                    href={getFilterUrl({ r: `${r}` })}
+                                >
+
+                                    {`${r} stars & up`}
+                                </Link>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
             <div className="md:col-span-4 space-y-4">
+                <div className="flex-between fex-col md:flex-row my-4">
+                    <div className="flex items-center">
+                        {
+                            q !== 'all' && q !== '' && 'Query: ' + q
+                        }
+                        {(category !== 'all' && category !== '') && 'Category: ' + category}
+                        {(price !== 'all') && ' Price: ' + price}
+                        {(rating !== 'all') && ' Rating: ' + rating + ' stars & up'}
+                        &nbsp;
+                        {(q !== 'all' && q !== '') || (category !== 'all' && category !== '') || (price !== 'all') || (rating !== 'all')
+                            ? <Button variant={'link'} asChild>
+                                <Link href='/search'>Clear</Link>
+                            </Button>
+                            : null}
+
+                    </div>
+                    <div>{/*SORT */}
+                        Sort by{' '}
+                        {sortOrders.map((s) => (
+                            <Link key={s} className={`mx-2 ${sort == s && 'font-bold'}`} href={getFilterUrl({ s })}>
+                                {s}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
                     {products.data.length === 0 && <div>No products found</div>}
                     {products.data.map((product) => (
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -100,7 +100,8 @@ export async function getAllProducts({
             ...priceFilter,
             ...ratingFilter
         },
-        orderBy: { createdAt: 'desc' },
+        orderBy: sort === 'lowest' ? { price: 'asc' } : sort === 'highest' ? { price: 'desc' }
+            : sort === 'rating' ? { rating: 'desc' } : { createdAt: 'desc' },
         skip: (page - 1) * limit,
         take: limit,
     });
